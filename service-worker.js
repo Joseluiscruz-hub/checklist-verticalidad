8b1d30a85cb9cca9c556728dd9878437b6e3ab75
Joseluiscruz-hub <Joseluiscruz-hub@users.noreply.github.com>
2025-10-26 16:14:31 +0000

    deploy: 39fd829cab80687ca570854379216054bcc4d6bb

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = 'checklist-v2';
 const urlsToCache = [
-  '/checklist-verticalidad/index.html',
+  'index.html',
+  'service-worker.js',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js',
